f748cfa85f0eead7cab3bc843ca35e0e48073856
SidMorrisMw <bis22-mnkhoma@mubas.ac.mw>
2025-08-06 10:14:41 +0200

    k

---
+++ Q2.js
@@ -0,0 +1,16 @@
+const PI=22/7;
+
+// PI=22/6;
+
+
+var name="Morris"; //has a global scope
+
+let name2="Martin";
+
+
+
+function print(){
+    console.log(name);
+    let age=12;
+    console.log(age);
+}
